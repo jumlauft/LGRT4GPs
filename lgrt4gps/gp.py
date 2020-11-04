f48a8e8cc356b980b07269f3271deb68e425e63e
Jonas Umlauft <jumlauft@gmail.com>
2020-11-04 10:56:40 +0100

    added input dimension checks gps

--- lgrt4gps/gp.py
+++ lgrt4gps/gp.py
@@ -6,10 +6,17 @@ def priorZero(x):
 
 class GP:
     def __init__(self, x, y, kernel, sigon=0.01, prior=priorZero):
-        self.X = x
+        # Check input
         n, dx = x.shape
+        y = y.reshape(-1,1)
+        if y.shape != (n,1) or dx != kernel.input_dim:
+            raise ValueError('dimension mismatch')
+
+
+        self.X = x
         self.kernel = kernel
         self.prior = prior
+        self.dx = dx
         if n > 0:
             K = kernel(x, x)
             self.L = cholesky(K + (sigon ** 2) * np.eye(n))
@@ -19,6 +26,9 @@ class GP:
 
 
     def predict(self, Xt):
+        if Xt.ndim == 2 != 2 or Xt.shape[1] != self.dx:
+            raise ValueError('dimension mismatch')
+
         if self.X.shape[0] > 0:
             Kxxt = self.kernel(self.X, Xt)
             mu = np.dot(self.alpha.reshape(-1), Kxxt).reshape(-1, 1) + self.prior(Xt)
--- tests/test_gp.py
+++ tests/test_gp.py
@@ -8,26 +8,34 @@ from lgrt4gps.kern import RBF
 def test_init():
     n, dx = 10, 2
     x = np.random.rand(n,dx)
-    y = np.random.rand(n)
-    gp = GP(x, y, RBF(dx))
+    y = np.random.rand(n,1)
+    try:
+        GP(x, y[:-1,:], RBF(dx))
+        GP(x, y, RBF(dx + 1))
+
+        # Should not get here
+        raise AssertionError
+    except ValueError:
+        pass
+
 
     n, dx = 0, 2
     x = np.empty((0,dx))
-    y = np.empty((0,))
+    y = np.empty((0,1))
     gp = GP(x, y, RBF(dx))
 
 def test_predict():
     nte = 20
     n, dx = 10, 2
     x = np.random.rand(n,dx)
-    y = np.random.rand(n)
+    y = np.random.rand(n,1)
     yte, s2 = GP(x, y, RBF(dx)).predict(np.random.rand(nte,dx))
     assert yte.shape == (nte,1)
     assert s2.shape == (nte,1)
 
     n, dx = 0, 2
     x = np.empty((0,dx))
-    y = np.empty((0,))
+    y = np.empty((0,1))
     yt, s2 = GP(x, y, RBF(dx)).predict(np.random.rand(nte,dx))
     ytz = np.zeros((nte,1))
     assert np.all(yt==ytz)
